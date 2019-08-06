c2e0fcf60a7afd3e6833b64ab4e5aa636b932f62
Paulo Lourenço <paulohll14@gmail.com>
2019-08-06 10:41:40 -0300

    Configuração scheme de teste + removido recordMode

--- github-bestsTests/GeneratedMocks.swift
+++ github-bestsTests/GeneratedMocks.swift
@@ -1,4 +1,4 @@
-// MARK: - Mocks generated from file: github-bests/Scenes/List/RepositoryListInteractor.swift at 2019-08-06 13:35:29 +0000
+// MARK: - Mocks generated from file: github-bests/Scenes/List/RepositoryListInteractor.swift at 2019-08-06 13:40:42 +0000
 
 //
 //  RepositoryListInteractor.swift
@@ -434,7 +434,7 @@ import UIKit
 }
 
 
-// MARK: - Mocks generated from file: github-bests/Scenes/List/RepositoryListPresenter.swift at 2019-08-06 13:35:29 +0000
+// MARK: - Mocks generated from file: github-bests/Scenes/List/RepositoryListPresenter.swift at 2019-08-06 13:40:42 +0000
 
 //
 //  RepositoryListPresenter.swift
@@ -858,7 +858,7 @@ import UIKit
 }
 
 
-// MARK: - Mocks generated from file: github-bests/Scenes/List/RepositoryListWorker.swift at 2019-08-06 13:35:29 +0000
+// MARK: - Mocks generated from file: github-bests/Scenes/List/RepositoryListWorker.swift at 2019-08-06 13:40:42 +0000
 
 //
 //  RepositoryListWorker.swift
